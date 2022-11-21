ae490c87aa60e82faf3ab97965280c12e8143900
sc0des <swannc0des@gmail.com>
2022-11-21 21:53:30 +0400

    removed ray

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,7 +10,6 @@ class WelcomeController extends Controller
 {
     public function __invoke(Request $request)
     {
-        ray('hello');
         $posts = Post::latest()->take(2)->get();
         $users = User::latest()->take(3)->get();
 
