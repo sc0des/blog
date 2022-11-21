3fca95574190fb1a79e2d2b269f8b34c35c06e96
sc0des <swannc0des@gmail.com>
2022-11-21 14:33:44 +0400

    admin user feature added

--- routes/web.php
+++ routes/web.php
@@ -35,18 +35,28 @@ require __DIR__.'/auth.php';
 
 // Logging Routes
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth','verified'])->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+});
+
+
+// Admin Routes
+
+Route::middleware(['auth','isAdmin'])->group(function () {
+
+});
 
 
 
-
-
+/*
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+});*/
 
 
