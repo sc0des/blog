f6c10ba9d2032992110041dee614a14f563152bb
sc0des <swannc0des@gmail.com>
2022-11-21 12:53:41 +0400

    replaced vite with tailwind and css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,12 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- Stylesheets -->
+        <link href="css/app.css" rel="stylesheet">
+
+        <!-- Tailwind -->
+        <script src="https://cdn.tailwindcss.com"></script>
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
